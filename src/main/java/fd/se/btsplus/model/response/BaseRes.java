31681bf54f21c61c98d207cebd386399b7b61456
VigilGLC <18302010038@fudan.edu.cn>
2021-03-27 14:57:26 +0800

    Fix BaseRes static Consctructor.

--- src/main/java/fd/se/btsplus/model/response/BaseRes.java
+++ src/main/java/fd/se/btsplus/model/response/BaseRes.java
@@ -1,13 +1,14 @@
 package fd.se.btsplus.model.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-@NoArgsConstructor(staticName = "empty")
-@AllArgsConstructor
+@NoArgsConstructor
 public class BaseRes {
+    public static BaseRes empty() {
+        return new BaseRes();
+    }
 }
